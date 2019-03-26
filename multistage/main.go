d889be78f9f55310c81b06602fb86c71b58ada67
John Lockwood <elitepropertiesbroker@gmail.com>
2019-03-26 19:36:11 -0400

    In multistage, report on hostname

--- multistage/main.go
+++ multistage/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -11,7 +12,11 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world."))
+		w.Write([]byte("<html><body><p>Hello world.</p>"))
+		if host, err := os.Hostname(); err == nil {
+			w.Write([]byte("<p>Served from " + host + ".</p>"))
+		}
+		w.Write([]byte("</body></html>"))
 	})
 
 	http.ListenAndServe(":9000", r)
